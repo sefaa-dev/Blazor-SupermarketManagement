0833fdd151a11588ae431ccf81626a303e3b186d
Sefa Yılmaz <sefayilmaz582@gmail.com>
2022-01-16 19:53:34 +0300

    first commit

---
+++ Plugins.DataStore.InMemory/CategoryInMemoryRepository.cs
@@ -0,0 +1,53 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UseCases.DataStorePluginInterfaces;
+
+namespace Plugins.DataStore.InMemory
+{
+    public class CategoryInMemoryRepository : ICategoryRepository
+    {
+        private List<Category> categories;
+
+        public CategoryInMemoryRepository()
+        {
+            categories = new List<Category>()
+            {
+                new Category {CategoryId = 1, Name = "Baverage", Description = "Baverage"},
+                new Category {CategoryId = 2, Name = "Bakery", Description = "Bakery"},
+                new Category {CategoryId = 3, Name = "Meat", Description = "Meat"},
+            };
+        }
+
+        public void AddCategory(Category category)
+        {
+            if (categories.Any(x => x.Name.Equals(category.Name, StringComparison.OrdinalIgnoreCase))) return;
+            var maxId = categories.Max(x => x.CategoryId);
+            category.CategoryId = maxId + 1;
+
+            categories.Add(category);
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            var categoryToUpdate = GetCategoryById(category.CategoryId); 
+            if (categoryToUpdate != null)
+            {
+                categoryToUpdate.Name = category.Name;
+                categoryToUpdate.Description = category.Description;
+            }
+                       
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return categories;
+        }
+
+        public Category GetCategoryById(int categoryId)
+        {
+            return categories?.FirstOrDefault(x => x.CategoryId == categoryId);
+        }
+    }
+}
