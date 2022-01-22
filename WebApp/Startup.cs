8f0cb93b77fd5710830c6c861a4c4389b9a38ce5
Sefa Yılmaz <sefayilmaz582@gmail.com>
2022-01-23 00:07:12 +0300

    sdf

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -47,7 +47,7 @@ namespace WebApp
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)   
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)    
         { 
             if (env.IsDevelopment())
             {
