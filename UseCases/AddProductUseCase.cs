5c093c31cb016bf51b003db679d8feffaa57a0c5
Sefa Yılmaz <sefayilmaz582@gmail.com>
2022-01-24 23:14:45 +0300

    sdf

---
+++ UseCases/AddProductUseCase.cs
@@ -0,0 +1,24 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.DataStorePluginInterfaces;
+
+namespace UseCases
+{
+    public class AddProductUseCase
+    {
+        private readonly IProductRepository productRepository;
+
+        public AddProductUseCase(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+        public void Execute(Product product)
+        {
+
+        }
+    }
+}
