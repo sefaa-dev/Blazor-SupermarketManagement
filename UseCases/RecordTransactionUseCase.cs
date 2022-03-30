08f2ee333f3562402252d00f11ed7846016a3553
Sefa Yılmaz <sefayilmaz582@gmail.com>
2022-03-30 17:54:46 +0300

    List of Sold İtems started

---
+++ UseCases/RecordTransactionUseCase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.DataStorePluginInterfaces;
+
+namespace UseCases
+{
+    public class RecordTransactionUseCase
+    {
+        private readonly ITransactionRepository transactionRepository;
+        private readonly IGetProductByIdUseCase getProductByIdUseCase;
+
+        public RecordTransactionUseCase(ITransactionRepository transactionRepository, IGetProductByIdUseCase getProductByIdUseCase)
+        {
+            this.transactionRepository = transactionRepository;
+            this.getProductByIdUseCase = getProductByIdUseCase;
+        }
+        public void Execute(int productId, int qty)
+        {
+            var product = getProductByIdUseCase.Execute(productId);
+            transactionRepository.Save(productId, product.Price.Value, qty);
+        }
+    }
+}
