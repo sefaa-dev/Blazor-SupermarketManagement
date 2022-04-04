9dd8543511a8dcd1bddde5363630ccbc082b4072
Sefa Yılmaz <sefayilmaz582@gmail.com>
2022-04-04 17:26:28 +0300

    Report tamam

--- WebApp/obj/Debug/net5.0/RazorDeclaration/Controls/TodayTransactionsComponent.razor.g.cs
+++ WebApp/obj/Debug/net5.0/RazorDeclaration/Controls/TodayTransactionsComponent.razor.g.cs
@@ -13,84 +13,84 @@ namespace WebApp.Controls
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 1 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 2 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 3 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 4 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 5 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 6 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 7 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 8 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 9 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using WebApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 10 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using WebApp.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 11 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using WebApp.Controls;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 13 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using CoreBusiness;
 
 #line default
@@ -104,7 +104,7 @@ using CoreBusiness;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 36 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\Controls\TodayTransactionsComponent.razor"
+#line 36 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\Controls\TodayTransactionsComponent.razor"
        
 
     private IEnumerable<Transaction> transactions;
--- WebApp/obj/Release/net5.0/RazorDeclaration/Pages/TransactionsReportComponent.razor.g.cs
+++ WebApp/obj/Release/net5.0/RazorDeclaration/Pages/TransactionsReportComponent.razor.g.cs
@@ -13,89 +13,90 @@ namespace WebApp.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 1 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 2 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 3 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 4 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 5 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 6 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 7 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 8 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 9 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using WebApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 10 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using WebApp.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 11 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using WebApp.Controls;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "C:\Users\Sefa\source\repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
+#line 13 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\_Imports.razor"
 using CoreBusiness;
 
 #line default
 #line hidden
 #nullable disable
+    [Microsoft.AspNetCore.Components.RouteAttribute("/transactions")]
     public partial class TransactionsReportComponent : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
@@ -103,6 +104,38 @@ using CoreBusiness;
         {
         }
         #pragma warning restore 1998
+#nullable restore
+#line 84 "C:\Users\Sefa\Source\Repos\Blazor-SupermarketManagement\WebApp\Pages\TransactionsReportComponent.razor"
+       
+
+    private string cashierName;
+    private DateTime startDate;
+    private DateTime endDate;
+    private IEnumerable<Transaction> transactions;
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        startDate = DateTime.Today;
+        endDate = DateTime.Today;
+    }
+
+    private void LoadTransactions()
+    {
+        transactions = GetTransactionsUseCase.Execute(cashierName, startDate, endDate);
+    }
+
+    private void PrintReport()
+    {
+        JSRuntime.InvokeVoidAsync("print");
+    }
+
+#line default
+#line hidden
+#nullable disable
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private UseCases.IGetTransactionsUseCase GetTransactionsUseCase { get; set; }
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private IJSRuntime JSRuntime { get; set; }
     }
 }
 #pragma warning restore 1591
